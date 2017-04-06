ed8c5ca223da8f6a63cbfc70fc31813d2dd45927
Jimmy K <jimmyk3_14159@gmail.com>
2017-04-05 23:27:26 -0500

    added rainbow.bat

---
+++ rainbow.bat
@@ -0,0 +1,19 @@
+@echo off
+:loop
+	color cb
+	call :wait
+	color e9
+	call :wait
+	color ad
+	call :wait
+	color bc
+	call :wait
+	color 9e
+	call :wait
+	color da
+	call :wait
+goto loop
+
+:wait
+	ping 1.1.1.1 -n 1 -w 300 > nul
+goto :EOF
